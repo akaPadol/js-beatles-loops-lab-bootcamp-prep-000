0982b5e6cb8063cef3f242ee64fdfbaac7f9bb2c
Antonino Badalamenti <bada.nino95@gmail.com>
2017-09-11 18:59:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 
-var ad=['po','pu']
-var an=['ca','ca']
-theBeatlesPlay(ad,an)
+
 function theBeatlesPlay(art,inst){
-  var res= new Array()
+  var res= []
 for (var i=0;i<art.lenght;++i){
   res.push(art[i]+" "+inst[i])
-  console.log(art[i]+" "+inst[i]);
+  
 }
 return res
-}
-
-function iLoveTheBeatles(n){
-  var res= new Array()
-
-  do {
-    res.push("I love the Beatles!")
-    ++n
-  } while (n<15);
-  return res
 }
 
 function johnLennonFacts(arr){
@@ -33,3 +21,14 @@ var n = 0
  }
  return arr
 }
+
+function iLoveTheBeatles(n){
+  var res= new Array()
+
+  do {
+    res.push("I love the Beatles!")
+    ++n
+  } while (n<15);
+  return res
+}
+
